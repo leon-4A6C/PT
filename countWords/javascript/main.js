ed4355f2310d872a95083572ecead5a3a60bbc4e
leon3110l <leon3110l@gmail.com>
2017-06-15 11:01:52 +0200

    added word and character count

--- countWords/javascript/main.js
+++ countWords/javascript/main.js
@@ -2,9 +2,11 @@ var output = document.getElementsByClassName("output")[0];
 
 document.getElementsByName("text")[0].addEventListener("keyup", e => {
   var text = e.target.value;
-  output.innerHTML = text;
+  var wordCount = text.split(" ").length-1;
+  var characterCount = text.split("").length;
+  output.innerHTML = text + "<br>Word count: " + wordCount + "<br>character count: " + characterCount;
 });
 
 document.getElementsByName("button")[0].addEventListener("click", e => {
-  
+
 });

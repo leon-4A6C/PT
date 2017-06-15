cae6be3d9acb30e969c84338337fd361a5ada29b
leon3110l <leon3110l@gmail.com>
2017-06-15 11:12:45 +0200

    found why it didn't work and fixed it

--- countWords/javascript/functions.js
+++ countWords/javascript/functions.js
@@ -1,7 +1,7 @@
 function firstCharacterUpper(str) {
   str = str.toLowerCase();
   str = str.split("");
-  str[0].toUpperCase();
+  str[0] = str[0].toUpperCase();
   var res = "";
   for (var i = 0; i < str.length; i++) {
     res += str[i];

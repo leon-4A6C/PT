63e4738d02a993583bce976988635aa6b8769b25
leon3110l <leon3110l@gmail.com>
2017-06-16 09:08:18 +0200

    made the first word upper case function better and added longest word and did stuff

--- countWords/javascript/functions.js
+++ countWords/javascript/functions.js
@@ -1,17 +1,29 @@
+// function firstCharacterUpper(str) {
+//   str = str.toLowerCase();
+//   str = str.trim().replace(/\s{2,}/g, ' ');
+//   var words = str.split(" ");
+//   var characters = [];
+//   var res = "";
+//   for (var i = 0; i < words.length; i++) {
+//     characters.push(words[i].split(""));
+//     characters[i][0] = characters[i][0].toUpperCase();
+//     words[i] = "";
+//     for (var j = 0; j < characters[i].length; j++) {
+//       words[i] += characters[i][j];
+//     }
+//     res += words[i] + " ";
+//   }
+//   return res
+// }
+
 function firstCharacterUpper(str) {
-  str = str.toLowerCase();
-  str = str.trim().replace(/\s{2,}/g, ' ');
+  return str.substr(0,1).toUpperCase() + str.substr(1);
+}
+
+function allFirstCharacterUpper(str) {
   var words = str.split(" ");
-  var characters = [];
-  var res = "";
   for (var i = 0; i < words.length; i++) {
-    characters.push(words[i].split(""));
-    characters[i][0] = characters[i][0].toUpperCase();
-    words[i] = "";
-    for (var j = 0; j < characters[i].length; j++) {
-      words[i] += characters[i][j];
-    }
-    res += words[i] + " ";
+    words[i] = firstCharacterUpper(words[i]);
   }
-  return res
+  return words.join(" ");
 }
--- countWords/javascript/main.js
+++ countWords/javascript/main.js
@@ -3,13 +3,24 @@ var output = document.getElementsByClassName("output")[0];
 document.getElementsByName("text")[0].addEventListener("keyup", e => {
   var text = e.target.value;
   text = text.trim().replace(/\s{2,}/g, ' ');
-  var wordCount = text.split(" ").length;
+  var words = text.split(" ");
+  var maxWord = {length: 0, word: ""};
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxWord.length) {
+      maxWord.length = words[i].length;
+      maxWord.word = words[i];
+    }
+  }
+  if (words[0] === "" && words.length === 1) {
+    words = [];
+  }
+  var wordCount = words.length;
   var characterCount = text.length;
-  output.innerHTML = text + "<br>Word count: " + wordCount + "<br>character count: " + characterCount;
+  output.innerHTML = text + "<br>Word count: " + wordCount + "<br>character count: " + characterCount + "<br>langste word lengte: " + maxWord.length + "<br>langste word: " + maxWord.word;
 });
 
 document.getElementsByName("button")[0].addEventListener("click", e => {
   var text = document.getElementsByName("text")[0].value;
-  text = firstCharacterUpper(text);
+  text = allFirstCharacterUpper(text);
   output.innerHTML += "<br><br>" + text;
 });
